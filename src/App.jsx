2f7a6a00fc44800012c89a1d52acc4e727c9f3a3
Pedro Gomes Marins <pedrogm.jf@gmail.com>
2020-10-02 13:13:14 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -8,23 +9,25 @@ import Card from './components/layout/Card'
 
 export default function App(props) {
     return (
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React</h1>
-            <Card titulo="Número Aleatório">
-                <Aleatorio max={100} min={10} />
-            </Card>
+            <div className='Cards'>
+                <Card titulo="Número Aleatório">
+                    <Aleatorio max={100} min={10} />
+                </Card>
 
-            <Card titulo="Fragmento">
-                <Fragmento />
-            </Card>
+                <Card titulo="Fragmento">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="Nota Aluno">
-                <ComParametro nome="ComParametro" nota={6} />
-            </Card>
+                <Card titulo="Nota Aluno">
+                    <ComParametro nome="ComParametro" nota={6} />
+                </Card>
 
-            <Card titulo="Primeiro">
-                <Primeiro />
-            </Card>
+                <Card titulo="Primeiro">
+                    <Primeiro />
+                </Card>
+            </div>
         </div>
     );
 }
