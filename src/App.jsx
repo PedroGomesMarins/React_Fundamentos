ee6dcfe20e0834d5be1bf19eb4df8763caddf94a
Pedro Gomes Marins <pedrogm.jf@gmail.com>
2020-10-06 15:15:18 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ParOuImpar from './components/condicional/ParOuImpar'
 import UsuarioInfo from './components/condicional/UsuarioInfo'
 import DiretaPai from './components/comunicacao/DiretaPai'
 import IndiretaPai from './components/comunicacao/IndiretaPai'
+import Input from './components/formulario/Input'
 
 export default function App(props) {
     return (
